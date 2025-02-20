b986f3ada66fcb602efad28abf677188cd3458f7
bilal160 <63717309+Bilal160@users.noreply.github.com>
2025-02-21 00:47:05 +0500

    updated 1.1

--- backend/src/validators/aiChat.validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_validator_1 = require("express-validator");
-const validatePrompt = [
-    (0, express_validator_1.check)("prompt")
-        .notEmpty()
-        .withMessage("Prompt is required")
-];
-exports.default = validatePrompt;
